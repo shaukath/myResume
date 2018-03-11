1b9b6e695d45ca6b7c9e87704153deb61e65f69d
shaukathpatan@gmail.com <shaukathpatan@gmail.com>
2018-03-11 22:19:06 +0530

    Mapping model into component, HTML and CSS design

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ResumeService } from './resume.service';
+import { Resume, Profile, Work, Skill, Education, Reference } from './resume';
 
 @Component({
   selector: 'app-resume',
@@ -8,7 +9,8 @@ import { ResumeService } from './resume.service';
 })
 export class ResumeComponent implements OnInit {
 
-resume: any;
+resume = new Resume();
+resumeData: any;
 
   constructor(private resumeService: ResumeService) { }
 
@@ -18,9 +20,77 @@ resume: any;
 
   getResume() {
     this.resumeService.getResumeDetails().subscribe(data => {
-      this.resume = data;
-      console.log(data);
-      console.log(this.resume.basics.name);
+      this.resumeData = data;
+      let profile: Profile;
+      let work: Work;
+      let skill: Skill;
+      let education: Education;
+      let reference: Reference;
+      this.resume.basics = this.resumeData.basics;
+      // console.log(data);
+      console.log(this.resumeData.basics.profiles);
+      if (this.resumeData.basics.profiles.length > 0) {
+        this.resumeData.basics.profiles.forEach(element => {
+          profile = new Profile();
+          profile.network = element.network;
+          profile.url = element.url;
+          profile.username = element.username;
+          this.resume.profiles.push(profile);
+          console.log(this.resume.profiles[0].username);
+        });
+      }
+
+      if (this.resumeData.work.length > 0) {
+        this.resumeData.work.forEach(element => {
+          work = new Work();
+          work.company = element.company;
+          work.position = element.position;
+          work.website = element.website;
+          work.startDate = element.startDate;
+          work.endDate = element.endDate;
+          work.summary = element.summary;
+          work.highlights = element.highlights;
+          this.resume.works.push(work);
+          console.log(element.highlights);
+          console.log(this.resume.works[0].highlights[1]);
+        });
+      }
+
+      if (this.resumeData.skills.length > 0) {
+        this.resumeData.skills.forEach(element => {
+          skill = new Skill();
+          skill.name = element.name;
+          skill.keywords = element.keywords;
+          this.resume.skills.push(skill);
+          console.log(element.keywords);
+          console.log(this.resume.skills[0].name);
+        });
+      }
+
+      if (this.resumeData.education.length > 0) {
+        this.resumeData.education.forEach(element => {
+          education = new Education();
+          education.institution = element.institution;
+          education.area = element.area;
+          education.studyType = element.studyType;
+          education.startDate = element.startDate;
+          education.endDate = element.endDate;
+          this.resume.education.push(education);
+          console.log(element.studyType);
+          console.log(this.resume.education[0].studyType);
+        });
+      }
+
+      if (this.resumeData.references.length > 0) {
+        this.resumeData.references.forEach(element => {
+          reference = new Reference();
+          reference.name = element.name;
+          reference.reference = element.reference;
+          this.resume.references.push(reference);
+          console.log(element.reference);
+          console.log(this.resume.references[0].reference);
+        });
+      }
     });
   }
 
--- src/app/resume/resume.ts
+++ src/app/resume/resume.ts
@@ -1,8 +1,92 @@
 export class Resume {
 
     basics: any;
+    profiles: Array<Profile> = new Array<Profile>();
+    works: Array<Work> = new Array<Work>();
+    skills: Array<Skill> = new Array<Skill>();
+    education: Array<Education> = new Array<Education>();
+    references: Array<Reference> = new Array<Reference>();
 
     constructor() {
-        this.basics = '';
+        this.basics = {
+            name: '',
+            label: '',
+            picture: '',
+            email: '',
+            phone: '',
+            website: '',
+            summary: '',
+            location: {
+                address: '',
+                city: '',
+            },
+            profiles: this.profiles
+        };
     }
 }
+
+export class Profile {
+    network: string;
+    username: string;
+    url: string;
+    constructor() {
+      this.network = '';
+      this.username = '';
+      this.url = '';
+    }
+  }
+
+  export class Work {
+    company: string;
+    position: string;
+    website: string;
+    startDate: string;
+    endDate: string;
+    summary: string;
+    highlights: Array<string> = new Array<string>();
+    constructor() {
+      this.company = '';
+      this.position = '';
+      this.website = '';
+      this.startDate = '';
+      this.endDate = '';
+      this.summary = '';
+      this.highlights = [];
+    }
+  }
+
+  export class Skill {
+    name: string;
+    level: string;
+    keywords: Array<string> = new Array<string>();
+    constructor() {
+      this.name = '';
+      this.level = '';
+      this.keywords = [];
+    }
+  }
+
+  export class Education {
+    institution: string;
+    area: string;
+    studyType: string;
+    startDate: string;
+    endDate: string;
+    constructor() {
+      this.institution = '';
+      this.area = '';
+      this.studyType = '';
+      this.startDate = '';
+      this.endDate = '';
+
+    }
+  }
+
+  export class Reference {
+    name: string;
+    reference: string;
+    constructor() {
+      this.name = '';
+      this.reference = '';
+    }
+  }
